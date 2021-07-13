9b823fb18383641349bfd3fb8dfea251c006290b
MikeyCouture <michelcouture42@gmail.com>
2021-07-13 16:53:49 -0400

    Context learning cont

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,19 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColourContext from "../contexts/ColourContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          ></i>
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          ></i>
-        </div>
-        <ColourContext.Provider value="primary">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColourContext.Provider value="primary">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColourContext.Provider>
+          </ColourContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
